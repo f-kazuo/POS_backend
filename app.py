0cd9a5150233ae6393105cf8ae87f086d2be0b1c
kazuofurui <kaz.furui@gmail.com>
2025-02-11 15:48:33 +0900

    Merge (app.py)

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://tech0-gen8-step4-pos-app-88.azurewebsites.net/"],
+    allow_origins=["https://tech0-gen8-step4-pos-app-87.azurewebsites.net/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
